1520b20942ffda99ce486364dbb19aadf1cb98cb
ShaineRosewel <shainematala@gmail.com>
2025-07-23 13:09:14 +0800

    Add r as arg to estimate_CDF and add calculate_variance fcn

--- R/algo1_helper.R
+++ R/algo1_helper.R
@@ -1,8 +1,10 @@
+calculate_variance <- function(moe) return((moe/1.645)^2)
+
 kde_cdf_function <- function(datapoints, bandwidth) {
   return(function(x) mean(pnorm((x - datapoints) / bandwidth)))
 }
 
-estimate_CDF <- function(dataset, i){
+estimate_CDF <- function(dataset, i, r){
   S = sqrt(dataset[i,'variance'])#sd(r[,i])
   IQR = quantile(r[,i], .75) - quantile(r[,i], .25)
   h_i <- 0.9*min(S, IQR/1.34)*(B^(1/5))
