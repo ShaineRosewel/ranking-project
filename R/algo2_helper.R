c6e8ef00a872bd7769018c41a998a641ff0e5476
ShaineRosewel <shainematala@gmail.com>
2025-07-26 15:05:49 +0800

    Create algo2_helper.R

---
+++ R/algo2_helper.R
@@ -0,0 +1,14 @@
+get_iter_misses <- function(K, file_list){
+  coverage_output_df <- lapply(file_list, function(file) {
+    df <- read_feather(file)
+    df$filename <- basename(file)
+    return(df)
+  }) %>%
+    bind_rows()
+  
+  subs <- coverage_output_df[,c('k', "theta_k", "theta", "kde_ci_lower", "kde_ci_upper", "filename")]
+  subs$theta_within <- (subs$theta >= subs$kde_ci_lower) & (subs$theta <= subs$kde_ci_upper)
+  subs$theta_k_within <- (subs$theta_k >= subs$kde_ci_lower) & (subs$theta_k <= subs$kde_ci_upper)
+  return(subs %>% select(c('k', "theta_k", "theta", "kde_ci_lower", "kde_ci_upper", "theta_within", "filename"))# %>% filter(!theta_within))
+  )
+}
