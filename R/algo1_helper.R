41afa8bd7f9dc00698ad5251d8448eb5fa079d94
ShaineRosewel <shainematala@gmail.com>
2025-07-30 11:30:11 +0800

    Align variable naming to pdf doc & add comment lines

--- R/algo1_helper.R
+++ R/algo1_helper.R
@@ -3,7 +3,6 @@ library("GoFKernel")
 calculate_variance <- function(moe) return((moe/1.645)^2)
 
 kde_cdf_function <- function(datapoints, h) {
-
   return(function(x) mean(pnorm((x - datapoints) / h)))
 }
 
@@ -51,67 +50,94 @@ get_ranks <- function(k, tuple_list){
   ))
 }
 
-get_t1 <- function(v){mean(v)}
+get_t1 <- function(v) mean(v)
 
-get_t2 <- function(v){prod(v)^(1/length(v))}
+get_t2 <- function(v) prod(v)^(1/length(v))
 
-get_t3 <- function(v){1 - ((length(v)+sum(v))/(length(v)^2))}
+get_t3 <- function(v) 1 - ((length(v)+sum(v))/(length(v)^2))
 
 run_algorithm1 <- function(B, dataset, seed = 4, alpha = 0.1) {
-  #dataset['variance'] <- lapply(dataset['moe_k'], calculate_variance)
+
   K <- dim(dataset)[1]
   set.seed(seed)
-  # step 1
-  set.seed(4)
+
+  # step 1 =====================================================================
+ 
+   set.seed(4)
   thetahat_star <- foreach(i = 1:K, .combine = cbind) %do% {
     foreach(b = 1:B, .combine = c) %do% {
-      rnorm(1, mean = dataset[i, 'theta_k'], sd = dataset[i, 'S'])
+      rnorm(1, 
+            mean = dataset[i, 'theta_k'], 
+            sd = dataset[i, 'S'])
     }
   }
-  colnames(thetahat_star) <- paste0("thetahat_star", sprintf("%02d", 1:K))
+  colnames(thetahat_star) <- paste0("thetahat_star", 
+                                    sprintf("%02d", 1:K))
+  theta_hat <- dataset$theta_k
   
-  #step 2
+  # step 2 =====================================================================
+  
+  sorted_indices <- order(dataset$theta_k) # index when order is increasing
+  sorted_dataset <- dataset[sorted_indices, ]
+  sorted_theta_hat <- sorted_dataset$theta_k
   sorted_thetahat_star <- t(apply(thetahat_star, 1, sort))
-  colnames(sorted_thetahat_star) <- paste0("sorted_thetahat_star", sprintf("%02d", 1:K))
-  #step 3
-  rstar <- sorted_thetahat_star - matrix(sort(t(dataset['theta_k'])),B, K, byrow=TRUE)
+  colnames(sorted_thetahat_star) <- paste0("sorted_thetahat_star", 
+                                           sprintf("%02d", 1:K))
+  
+  # step 3 =====================================================================
+  
+  rstar <- sorted_thetahat_star - matrix(sorted_theta_hat,B, K, byrow=TRUE)
   colnames(rstar) <- paste0("rstar", sprintf("%02d", 1:K))
-  # step 4
+  
+  # step 4 =====================================================================
+  
   Fhat <- lapply(1:K, function(x)(estimate_CDF(x, rstar, B)))
-  # step 5
+  
+  # step 5 =====================================================================
+  
   Ystar <- foreach(i = 1:K, .combine = cbind) %do% {
     sapply(rstar[,i], Fhat[[i]])
   }
   colnames(Ystar) <- paste0("Ystar", sprintf("%02d", 1:K))
-  # step 6
-  Ustar <- apply(apply(Ystar, MARGIN = c(1, 2), FUN = get_inner_max), 1, max)
-  # step 7
+  
+  # step 6 =====================================================================
+  Ustar <- apply(
+    apply(Ystar, 
+          MARGIN = c(1, 2), 
+          FUN = get_inner_max), 
+    1, 
+    max)
+  
+  # step 7 =====================================================================
+  
   uhat <- quantile(Ustar, probs = 1 - alpha)
-  # step 8
-  sorted_indices <- order(dataset$theta_k)
-  sorted_dataset <- dataset[sorted_indices, ]
-  sorted_theta_k <- sorted_dataset$theta_k
   
-  Fhat.inv <- lapply(Fhat, function(F) inverse(F, lower = -100, upper = 100))
+  # step 8 =====================================================================
   
-  sorted_dataset$Fhat.inv_u    <- sapply(1:K, function(i) Fhat.inv[[i]](uhat))
-  sorted_dataset$Fhat.inv_1_u  <- sapply(1:K, function(i) Fhat.inv[[i]](1 - uhat))
+  Fhat.inv <- lapply(Fhat, 
+                     function(F) inverse(F, lower = -100, upper = 100))
+  sorted_dataset$Fhat.inv_u    <- sapply(1:K, 
+                                         function(i) Fhat.inv[[i]](uhat))
+  sorted_dataset$Fhat.inv_1_u  <- sapply(1:K, 
+                                         function(i) Fhat.inv[[i]](1 - uhat))
   
-  sorted_dataset$kde_ci_lower <- sorted_theta_k - sorted_dataset$Fhat.inv_u
-  sorted_dataset$kde_ci_upper <- sorted_theta_k - sorted_dataset$Fhat.inv_1_u
-  
+  sorted_dataset$kde_ci_lower <- sorted_theta_hat - sorted_dataset$Fhat.inv_u
+  sorted_dataset$kde_ci_upper <- sorted_theta_hat - sorted_dataset$Fhat.inv_1_u
   sorted_dataset$original_index <- sorted_indices
   
   interval_table <- sorted_dataset %>%
     select(k = k,
            theta_k = theta_k,
            kde_ci_lower = kde_ci_lower,
-           kde_ci_upper = kde_ci_upper)
+           kde_ci_upper = kde_ci_upper,
+           original_index = original_index)
+
+  # END ------------------------------------------------------------------------
 
   return(list(interval_table = interval_table, 
-              Finv_u = sapply(Fhat.inv, function(x)x(uhat)),
-              Finv_1_u = sapply(Fhat.inv, function(x)x(1-uhat)),
-              sorted_thetahat = sorted_theta_k, # sampled thetahats
+              Finv_u = sorted_dataset$Fhat.inv_u,
+              Finv_1_u = sorted_dataset$Fhat.inv_1_u,
+              sorted_thetahat = sorted_theta_hat,
               sorted_indices = sorted_indices,
               Fhat = Fhat,
               uhat = uhat
