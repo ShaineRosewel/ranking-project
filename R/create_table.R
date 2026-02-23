ed55fb1261e3d6643526b4b38ef92d2ad3eed3ca
ShaineRosewel <shainematala@gmail.com>
2026-02-23 08:55:38 +0800

    Adjust table formatting for FALSE

--- R/create_table.R
+++ R/create_table.R
@@ -27,12 +27,13 @@ create_table_for_tightness_measure <- function(summary, metric_type,
   if (equicorrelation) {
     vector_1 <- c("K", "r")
     striped <- rep(c(0, 6, 12), each = 3) + 1:3
+    headers <- c(" "=2,"Low" = 3, "Med" = 3, "High" = 3)
   } else {
-    vector_1 <- c("K", "r", "blocks")
+    vector_1 <- c("K")
     striped <- rep(c(0, 10), each = 5) + 1:5
-  }
-
-  return(summary  %>%
+    headers <- c(" "=1,"Low" = 3, "Med" = 3, "High" = 3)
+  
+  formatted <- summary  %>%
     kable("latex",
           booktabs = TRUE,
           escape = FALSE,
@@ -48,7 +49,18 @@ create_table_for_tightness_measure <- function(summary, metric_type,
                   font_size=11.5,
                   stripe_color = "gray!15",
                   stripe_index = rep(c(0, 6, 12), each = 3) + 1:3) %>%
-    add_header_above(c(" "=2,"Low" = 3, "Med" = 3, "High" = 3))
+    add_header_above(headers)
+
+  if (equicorrelation) {
+    final <- formatted
+  } else {
+    final <- formatted %>% pack_rows("2 balanced blocks", 1, 5) %>%
+      pack_rows("2 unbalanced blocks", 6, 10) %>%
+      pack_rows("3 unbalanced blocks", 11, 15)
+  }
+  }
+  return(
+    final
   )
 }
 
