27b4069f5f79df9789287a8fa87d7577b667a582
ShaineRosewel <shainematala@gmail.com>
2026-03-15 17:33:45 +0800

    Add result plot for Pulse

--- R/create_plot.R
+++ R/create_plot.R
@@ -1,5 +1,6 @@
-library("ggplot2")
-library("dplyr")
+library(ggplot2)
+library(dplyr)
+library(tidyr)
 
 create_boxplot_for_true_theta <- function(dataset){
   return(
@@ -15,4 +16,35 @@ create_boxplot_for_true_theta <- function(dataset){
       theme_bw() +
       theme(plot.title = element_text(hjust = 0.5))
   )
+}
+
+create_plot_for_pulse <- function(dat_to_plot){
+  
+  data_to_plot <- dat_to_plot %>% filter(Approach != 'pulse') #%>% filter(Approach != 'independent')
+  data_to_plot$Approach <- factor(data_to_plot$Approach, levels = c('independent', 'bonferroni', 'nonrank'))
+  
+  cap_first <- function(x) {
+    paste0(toupper(substring(x, 1, 1)), substring(x, 2))
+  }
+  
+  
+  p <- ggplot(data = data_to_plot, aes(x = reorder(Candidate, -k),
+                                       y = as.numeric(string_ranks))) + 
+    geom_point(data = subset(data_to_plot, highlight0 == "yes"), shape = 16, size = 1, color = "black") +
+    geom_point(data = subset(data_to_plot, highlight0 == "no"), shape = 4, size = 0.25, aes(color = highlight1)) + 
+    scale_color_manual(values = c("DuterTen" = "limegreen", "Alyansa" = "red", "KiBam" = "gold", "Makabayan" = "blue","DuterTen-Alyansa" = "black")) +
+    scale_y_continuous(breaks = seq(1, max(prepared_data$k), by = 4)) +
+    labs(title='95% Joint Confidence Region for Candidates with at least 1% Votes') + xlab('Candidate') + ylab('Sample rank') +
+    theme_bw() +
+    theme(axis.text.x = element_text(hjust = 1, size = 5),
+          axis.text.y = element_text(size=5)
+    ) +
+    guides(colour="none") +
+    theme(axis.line = element_line(colour = "gray"),
+          panel.grid.minor = element_blank(),
+          panel.grid.major.x = element_blank(),
+          text = element_text(family = "serif")) +
+    facet_wrap(~Approach, ncol=length(unique(data_to_plot$Approach)), 
+               labeller = as_labeller(cap_first)) + coord_flip()
+  return(p)
 }
