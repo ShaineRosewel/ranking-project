0a3958e3b3cd7ca81bdb6fda0c490fe74ace69ab
ShaineRosewel <shainematala@gmail.com>
2026-03-21 17:06:21 +0800

    Remove commented codes

--- R/create_table.R
+++ R/create_table.R
@@ -153,121 +153,6 @@ create_side_by_side_table_within_main <- function(eq_data,
 }
 
 
-# create_table_for_tightness_measure <- function(summary, metric_type,
-#                                                equicorrelation){
-#   
-#   # if (unordered) {
-#   unord <- c("Ind","Bonf","NR")
-#   ord <- c("Asymp","Boot")
-#   colwidth  <- "0.8cm"
-#   # } else {
-#   #   selected_columns <- c("Asymptotic Var","Bootstrap Est")
-#   #   colwidth  <- "3cm"
-#   # }
-#   
-#   num <- length(c(ord,unord))
-#   
-#   # sd <- c("Low $\\\\boldsymbol{\\\\theta}$ Variance" = 3, 
-#   #         "Moderate $\\\\boldsymbol{\\\\theta}$ Variance" = 3, 
-#   #         "High $\\\\boldsymbol{\\\\theta}$ Variance" = 3,
-#   #         "Low $\\\\boldsymbol{\\\\theta}$ Variance" = 2, 
-#   #         "Moderate $\\\\boldsymbol{\\\\theta}$ Variance" = 2, 
-#   #         "High $\\\\boldsymbol{\\\\theta}$ Variance" = 2)
-#   
-#   sd <- c("Low" = 3, 
-#           "Moderate" = 3, 
-#           "High" = 3,
-#           "Low" = 2, 
-#           "Moderate" = 2, 
-#           "High" = 2)
-#   approach <- c(" " = 2,c("Unordered" = 9, "Ordered" = 6))
-#   
-#   if (equicorrelation) {
-#     vector_1 <- c("K", "r")
-#     striped <- c(2:4, 8:10, 14:16)#rep(c(0, 6, 12), each = 3) + 1:3
-#     headers <- c(" " = 2, sd)
-#   } else {
-#     vector_1 <- c("K")
-#     striped <- -1 #rep(c(0, 10), each = 5) + 1:5
-#     headers <- c(" "= 1, sd)
-#   }
-# 
-# # create_table_for_tightness_measure <- function(summary, metric_type,
-# #                                                equicorrelation,
-# #                                                unordered = TRUE){
-# # 
-# #   if (unordered) {
-# #     selected_columns <- c("Independent","Bonferroni","Nonrank")
-# #     colwidth  <- "2cm"
-# #   } else {
-# #     selected_columns <- c("Asymptotic Var","Bootstrap Est")
-# #     colwidth  <- "3cm"
-# #   }
-# #   num <- length(selected_columns)
-# #   if (equicorrelation) {
-# #     vector_1 <- c("K", "r")
-# #     striped <- rep(c(0, 6, 12), each = 3) + 1:3
-# #     headers <- c(" " = 2, 
-# #                  "Low $\\\\boldsymbol{\\\\theta}$ Variance" = num, 
-# #                  "Moderate $\\\\boldsymbol{\\\\theta}$ Variance" = num, 
-# #                  "High $\\\\boldsymbol{\\\\theta}$ Variance" = num)
-# #   } else {
-# #     vector_1 <- c("K")
-# #     striped <- -1 #rep(c(0, 10), each = 5) + 1:5
-# #     headers <- c(" "=1,
-# #                  "Low $\\\\boldsymbol{\\\\theta}$ Variance" = num, 
-# #                  "Moderate $\\\\boldsymbol{\\\\theta}$ Variance" = num,
-# #                  "High $\\\\boldsymbol{\\\\theta}$ Variance" = num)
-# #   }
-# 
-#   
-#   formatted <- summary %>%
-#     kable("latex",
-#           booktabs = TRUE,
-#           escape = FALSE,
-#           linesep = "",
-#           longtable = TRUE,
-#           col.names = c(vector_1, 
-#                         c(rep(unord,3),rep(ord,3))),
-#           caption = paste("Simulation Results for Tightness Measure", 
-#                           metric_type)) %>%
-#     collapse_rows(columns = 1,
-#                   valign = "middle", 
-#                   latex_hline = "none",
-#                   row_group_label_position = "first") %>%
-#     add_header_above(headers, escape = FALSE) %>%
-#     add_header_above(approach, escape = FALSE) %>%
-#     kable_styling(latex_options = c("striped",
-#                                     "repeat_header",
-#                                     "HOLD_position"),
-#                   font_size=FONT_SIZE-1,
-#                   stripe_color = "gray!15",
-#                   stripe_index = striped)
-#                     #rep(c(0, 6, 12), each = 3) + 1:3) %>%
-# 
-#   if (equicorrelation) {
-#     final <- formatted %>% 
-#       column_spec(1:2, width = "0.5cm") %>%
-#       column_spec(3:(2+3*num), width = colwidth)
-#   } else {
-#     final <- formatted %>% 
-#       column_spec(1:1, width = "1cm") %>%
-#       column_spec(2:(1+3*num), width = colwidth) %>%
-#       pack_rows("2 balanced blocks", 1, 5,
-#                 latex_gap_space = "0.3em") %>%
-#       pack_rows("2 unbalanced blocks", 6, 10,
-#                 latex_gap_space = "0.3em") %>%
-#       pack_rows("3 unbalanced blocks - low", 11, 15,
-#                 latex_gap_space = "0.3em") %>%
-#       pack_rows("3 unbalanced block - high", 16, 20,
-#                 latex_gap_space = "0.3em")
-#   }
-#   return(
-#     final
-#   )
-# }
-
-
 create_table_for_tightness_measure <- function(summary, metric_type,
                                                unordered = TRUE){
   
@@ -361,7 +246,6 @@ create_table_for_tightness_measure <- function(summary, metric_type,
 }
 
 
-
 create_table_for_coverage <- function(summary, footnote, equicorrelation = TRUE,
                                       unordered = TRUE){
   
@@ -425,7 +309,3 @@ create_table_for_coverage <- function(summary, footnote, equicorrelation = TRUE,
              escape =FALSE)
   )
 }
-
-# https://stackoverflow.com/questions/66472411/how-to-reduce-size-of-footnote-in-kableextra-latex
-
-
